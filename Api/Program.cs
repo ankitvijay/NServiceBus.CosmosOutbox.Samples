d4fd9f15bd894746cb4d58a127a1aa245e220a17
AnkitVijay <ankit.vj.smit@gmail.com>
2021-08-22 10:03:06 +1000

    Converted to minmal API

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,33 +1,60 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using NServiceBus;
 using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace NServiceBus.CosmosOutbox.Samples.Api
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseNServiceBus(context =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var endpointConfiguration = new EndpointConfiguration("Samples.Api");
+    var transport = endpointConfiguration.UseTransport<LearningTransport>();
+    var routing = transport.Routing();
+    routing.RouteToEndpoint(Assembly.Load("Messages"), "Samples.Worker");
+    endpointConfiguration.SendOnly();
+    return endpointConfiguration;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-                .UseNServiceBus(context =>
-                {
-                    var endpointConfiguration = new EndpointConfiguration("Samples.Api");
-                    var transport = endpointConfiguration.UseTransport<LearningTransport>();
-                    var routing = transport.Routing();
-                    routing.RouteToEndpoint(Assembly.Load("Messages"), "Samples.Worker");
-                    endpointConfiguration.SendOnly();
-                    return endpointConfiguration;
-                });
-    }
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Posts Api", Version = "v1" });
+            });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection()
+   .UseStaticFiles()
+   .UseSwagger()
+   .UseSwaggerUI(options => { options.SwaggerEndpoint("v1/swagger.json", "Posts Api"); })
+   .UseRouting();
+
+app.MapPost("/create", async(IMessageSession messageSession, [FromBody] Post post) =>
+{
+    await messageSession.Send(new Messages.Post(post.Topic, post.Description));
+    return Results.Accepted();
+});
+
+app.MapPost("/add-comment", async (IMessageSession messageSession, [FromBody] Post post) =>
+{
+    await messageSession.Send(new Messages.Post(post.Topic, post.Description));
+    return Results.Accepted();
+});
+
+app.Run();
+
+public record Post(string Topic, string Description);
+public record Comment(string PostId, string Content);
+
+
